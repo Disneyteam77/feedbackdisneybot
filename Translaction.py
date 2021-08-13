f7c96bdbbb27e7d9d361c85c35f1b31807ef4163
Disneyteam <87754364+Disneyteam77@users.noreply.github.com>
2021-08-13 18:22:47 +0530

    Create Translaction.py

---
+++ Translaction.py
@@ -0,0 +1,21 @@
+from config import Config
+
+class Translation(object):
+      START = str(Config.START) + "\n\nHey , welcome to Disney Team Feedback Bot"
+
+      RULES = Config.RULES
+
+      LOGIN = """Only admins can receive feedbacks"""
+
+
+      ABOUT = """**MY DETAILS:**
+```🤖My Name:``` [Disneyteamfeedbackbot](https://t.me/disneyteamfeedbackbot)
+    
+```📝 Language:``` [Python 3](https://www.python.org/)
+```🧰 Framework:``` [Pyrogram](https://github.com/pyrogram/pyrogram)
+```👨🏻‍💻 Developer:``` [Doreamonfans](https://t.me/doreamonfans1)
+               
+```📢 Channel:``` [Disney Team](https://t.me/disneygrou)
+```👥 Group:``` [Disney Team Chat](https://t.me/disneyteamchat)
+Inspired By @doreamonfans1
+"""
